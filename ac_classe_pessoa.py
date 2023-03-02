848dbdae6d5f330675ba5f1db73114cf703ac77a
limav1c <103318987+limav1c@users.noreply.github.com>
2023-03-02 18:53:12 -0300

    Update ac_classe_pessoa.py

--- ac_classe_pessoa.py
+++ ac_classe_pessoa.py
@@ -3,6 +3,17 @@ class Pessoa:
         self.nome = nome
         self.email = email
         self.telefone = telefone
+        
     def __str__(self):
         return f'{self.nome}, '+\
                f'{self.email}, {self.telefone}'
+    
+    #CAPACIDADE DE SE TRANSFORMAR EM FORMATO JSON
+    
+    def json(self):
+        return {
+            "nome" : self.nome,
+            "email" : self.email,
+            "telefone" : self.telefone 
+        }
+    
